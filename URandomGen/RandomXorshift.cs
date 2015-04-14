09b5eaa889442c3fe567403d4169e7992b9dcfd0
KimikoMuffin <kimiko.muffin@gmail.com>
2015-04-14 11:36:57 -0400

    Renamed RandomXorShift to RandomXorshift; added LICENSE-ThirdParty.md; and updated the readme to contain actual useful information.

--- URandomGen/RandomXorshift.cs
+++ URandomGen/RandomXorshift.cs
@@ -1,14 +1,44 @@
-﻿using System;
+﻿#region BSD license
+/*
+Copyright © 2015, KimikoMuffin
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+This file uses code derived from the content of the Wikipedia article
+http://en.wikipedia.org/wiki/Xorshift
+This article is released under a Creative Commons Attribution Share-Alike license.
+See http://creativecommons.org/ for more information.
+*/
+#endregion
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace URandomGen
 {
     /// <summary>
     /// A random number generator based on the xorshift* algorithm.
     /// </summary>
-    public class RandomXorShift : RandomGen
+    public class RandomXorshift : RandomGen
     {
         private const int _seedCount = 32;
         private const int _seedMask = 31;
@@ -23,7 +53,7 @@ namespace URandomGen
         /// <exception cref="ArgumentNullException">
         /// <paramref name="seeds"/> is <c>null</c>.
         /// </exception>
-        public RandomXorShift(IEnumerable<uint> seeds)
+        public RandomXorshift(IEnumerable<uint> seeds)
         {
             SeedArray = new uint[_seedCount];
             uint curCount = CopyToArray(seeds, SeedArray);
@@ -52,7 +82,7 @@ namespace URandomGen
         /// <exception cref="ArgumentNullException">
         /// <paramref name="seeds"/> is <c>null</c>.
         /// </exception>
-        public RandomXorShift(params uint[] seeds)
+        public RandomXorshift(params uint[] seeds)
             : this((IEnumerable<uint>)seeds)
         {
         }
@@ -61,7 +91,7 @@ namespace URandomGen
         /// Creates a new instance using the specified seed.
         /// </summary>
         /// <param name="seed">A 32-bit seed used to initialize the random number generator.</param>
-        public RandomXorShift(uint seed)
+        public RandomXorshift(uint seed)
             : this((IEnumerable<uint>)new uint[] { seed })
         {
         }
@@ -69,7 +99,7 @@ namespace URandomGen
         /// <summary>
         /// Creates a new instance using <see cref="RandomGen.DefaultSeeds()"/>.
         /// </summary>
-        public RandomXorShift()
+        public RandomXorshift()
             : this((IEnumerable<uint>)DefaultSeeds())
         {
         }
